ba24260e7e40e1cd39cbc269bbb71bca5c77a753
Hellen2atsunga <atsungahellen@gmail.com>
2024-02-08 00:43:41 -0800

    inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+se_geometry.py
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+        """Represent base geometry."""
+
+            def area(self):
+                        """Not implemented."""
+                                raise Exception("area() is not implemented")
